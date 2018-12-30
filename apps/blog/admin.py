a20adf484bb35420cf8fa136e8c549a60a6e7ae5
alejandrohdo <ale.1010@live.com>
2018-12-29 22:51:10 -0500

    Funcionalidades del Blog integrado

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from import_export import resources
-from .models import Blog, Categoria
+from .models import Blog, Categoria, ContadorVisita
 from import_export.admin import ImportExportModelAdmin
 from pagedown.widgets import AdminPagedownWidget
 from django.db import models
@@ -14,7 +14,7 @@ class Blog_resource(resources.ModelResource):
         exclude = ()
 
 class Blog_admin(ImportExportModelAdmin):
-	filter_horizontal = ('categorias',)
+	filter_horizontal = ('categorias', 'posts_relacionados')
 	search_fields = ('titulo','resumen',
 				'fecha_publicacion','autor__perfil_usuario__nombres',
 				'autor__perfil_usuario__apellidos', 'palabras_clave')
@@ -38,6 +38,20 @@ class Categoria_resource(resources.ModelResource):
 class Categoria_admin(ImportExportModelAdmin):
     resource_class = Categoria_resource
 
+
+class ContadorVisita_resource(resources.ModelResource):
+
+    class Meta:
+        model = ContadorVisita
+        exclude = ()
+
+
+class ContadorVisita_admin(ImportExportModelAdmin):
+	list_display = ('blog','contador','fecha_creacion')
+	search_fields = ('blog__titulo',)
+	resource_class = ContadorVisita_resource
+
 admin.site.register(Categoria, Categoria_admin)
 admin.site.register(Blog, Blog_admin)
+admin.site.register(ContadorVisita, ContadorVisita_admin)
 
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -18,7 +18,7 @@ class Categoria(models.Model):
 class Blog(models.Model):
     titulo = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(editable=False, null=True, max_length=250)
-    resumen = models.TextField(null=True)
+    resumen = models.TextField(null=True, max_length=80)
     contenido = models.TextField()
     categorias = models.ManyToManyField(Categoria)
     portada = models.ImageField(upload_to='portadas-blog', null=True)
@@ -27,6 +27,8 @@ class Blog(models.Model):
     vistas = models.PositiveIntegerField(default=0, blank=True, null=True)
     palabras_clave = models.CharField(max_length=800, null=True, blank=True) 
     estado = models.BooleanField(default=True)
+    posts_relacionados = models.ManyToManyField("self",
+                                     verbose_name=("Posts relacionados"), blank=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
@@ -35,3 +37,12 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.titulo or u''
+
+class ContadorVisita(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    contador = models.IntegerField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.blog, self.contador) or u''
